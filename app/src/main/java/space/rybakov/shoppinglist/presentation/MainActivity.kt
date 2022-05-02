3563c120d032ecb82002e789386dc6f5ef563f77
dmitry <dimryb@bk.ru>
2022-05-02 22:50:26 +0700

    ShopList теперь liveData на который можно подписаться

--- app/src/main/java/space/rybakov/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/space/rybakov/shoppinglist/presentation/MainActivity.kt
@@ -9,13 +9,20 @@ import space.rybakov.shoppinglist.R
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
 
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             Log.d("MainActivityTest", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+                //viewModel.deleteShopList(item)
+            }
         }
-        viewModel.getShopList()
     }
 }
