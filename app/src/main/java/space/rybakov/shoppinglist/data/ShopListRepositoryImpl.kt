03fb4edf2124a90fc29e1356279902e026c10282
dmitry <dimryb@bk.ru>
2022-05-03 19:36:36 +0700

    В RecyclerView Отображается весь список

--- app/src/main/java/space/rybakov/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/space/rybakov/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -6,22 +6,23 @@ import androidx.lifecycle.MutableLiveData
 import space.rybakov.shoppinglist.domain.ShopItem
 import space.rybakov.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = sortedSetOf<ShopItem>({o1, o2 -> o1.id.compareTo(o2.id)})
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10){
-            val item = ShopItem("Name $i", i, true)
+        for (i in 0 until 1000) {
+            val item = ShopItem("Name ${i + 1}", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
 
     override fun addShopItem(shopItem: ShopItem) {
-        if(shopItem.id == ShopItem.UNDEFINED_ID){
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
@@ -49,7 +50,7 @@ object ShopListRepositoryImpl : ShopListRepository {
         return shopListLD
     }
 
-    private fun updateList(){
+    private fun updateList() {
         shopListLD.value = shopList.toList()
     }
 }
