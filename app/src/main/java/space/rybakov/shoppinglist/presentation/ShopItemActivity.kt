7e224de33b4243517fae99b69daba5cb9111ff70
dmitry <dimryb@bk.ru>
2022-05-13 00:16:05 +0700

    Prepare ShopItemActivity

---
+++ app/src/main/java/space/rybakov/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package space.rybakov.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import space.rybakov.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
