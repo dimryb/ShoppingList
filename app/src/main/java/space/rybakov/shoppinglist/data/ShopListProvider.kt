0c629be1dc2c919402b281c15e862a5daf1c5794
dmitry <dimryb@bk.ru>
2022-07-19 01:05:51 +0700

    Использование insert у ContentProvider и применение ContentValues

--- app/src/main/java/space/rybakov/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/space/rybakov/shoppinglist/data/ShopListProvider.kt
@@ -6,6 +6,7 @@ import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
+import space.rybakov.shoppinglist.domain.ShopItem
 import space.rybakov.shoppinglist.presentation.ShopApplication
 import javax.inject.Inject
 
@@ -17,6 +18,9 @@ class ShopListProvider: ContentProvider() {
 
     @Inject
     lateinit var shopListDao: ShopListDao
+
+    @Inject
+    lateinit var mapper: ShopListMapper
 
     private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
         addURI("space.rybakov.shoppinglist", "shop_items", GET_SHOP_ITEM_QUERY)
@@ -49,8 +53,24 @@ class ShopListProvider: ContentProvider() {
         TODO("Not yet implemented")
     }
 
-    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        when (uriMatcher.match(uri)){
+            GET_SHOP_ITEM_QUERY -> {
+                if (values == null) return null
+                val id = values.getAsInteger("id")
+                val name = values.getAsString("name")
+                val count = values.getAsInteger("count")
+                val enabled = values.getAsBoolean("enabled")
+                val shopItem = ShopItem(
+                    id = id,
+                    name = name,
+                    count = count,
+                    enabled = enabled
+                )
+                shopListDao.addShopItemSync(mapper.mapEntityToDbModel(shopItem))
+            }
+        }
+        return null
     }
 
     override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
